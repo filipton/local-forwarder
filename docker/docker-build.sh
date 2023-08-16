dd6af6336e95f7f2d3d2aaf5d54fee367dbff4fc
filipton <filipton12@gmail.com>
2023-08-16 23:28:44 +0200

    chore: docker publish

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cwd=$(pwd)
+echo "Enter the version number: "
+read version
 
 cd ../client
 cargo build -r
@@ -11,3 +13,16 @@ cd ../server
 cargo build -r
 docker build -t filipton/lf-server -f .Dockerfile .
 cd $cwd
+
+docker tag filipton/lf-client filipton/lf-client:$version
+docker tag filipton/lf-server filipton/lf-server:$version
+
+docker push filipton/lf-client:$version
+docker push filipton/lf-server:$version
+
+docker tag filipton/lf-client filipton/lf-client:latest
+docker tag filipton/lf-server filipton/lf-server:latest
+
+docker push filipton/lf-client:latest
+docker push filipton/lf-server:latest
+
